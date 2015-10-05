93460d9d22c48cbb018be32cb308358babbd6510
Cichol <cichol@live.cn>
2015-10-05 15:15:51 +0800

    add rm_main

---
+++ main.rb
@@ -0,0 +1,205 @@
+#encoding: utf-8
+
+class Game_Message
+  attr_accessor :continue, :selecting_keyword, :current_keyword
+
+  def continue?
+    @continue
+  end
+
+  def selecting_keyword?
+    @selecting_keyword
+  end
+
+  def keyword_choice?
+    @selecting_keyword
+  end
+
+  def busy?
+    has_text? || choice? || num_input? || item_choice? || keyword_choice?
+  end
+end
+
+class Window_KeyWordItem < Window_ItemList
+  def initialize(message_window)
+    @message_window = message_window
+    super(0, 0, Graphics.width, fitting_height(10))
+    self.openness = 0
+    deactivate
+    set_handler(:ok,     method(:on_ok))
+    set_handler(:cancel, method(:on_cancel))
+  end
+
+  def start
+    refresh
+    select(0)
+    open
+    activate
+  end
+
+  def current_item_enabled?
+    true
+  end
+
+  def on_ok
+    result = item
+    $game_message.current_keyword = result
+    $game_message.selecting_keyword = false
+    close
+  end
+
+  def on_cancel
+    p 1
+    $game_message.current_keyword = nil
+    $game_message.selecting_keyword = false
+    close
+  end
+
+  def make_item_list
+    @data = ["t1", "t2", "t3"]
+    @data.push(nil) if include?(nil)
+  end
+
+  def draw_item(index)
+    item = @data[index]
+    if item
+      rect = item_rect(index)
+      rect.width -= 4
+      draw_text_ex(rect.x, rect.y, item)
+    end
+  end
+end
+
+
+class Window_MessageName < Window_Base
+  def initialize(message_window)
+    @message_window = message_window
+    super(0, 0, window_width, fitting_height(1))
+    self.openness = 0
+  end
+  
+  def window_width
+    150
+  end
+  
+  def draw_actor_name(name)
+    contents.clear
+    if name != ''
+      self.visible = true
+      draw_text_ex 0, 0, name
+    else
+      self.visible = false
+    end
+  end
+
+  def update
+    super
+    self.x = @message_window.x
+    self.y = @message_window.y - height
+  end
+end
+
+class Window_Message
+  
+  def visible_line_number
+    4
+  end
+  
+  alias :old_create_all_windows :create_all_windows
+  def create_all_windows
+    old_create_all_windows
+    @keyword_window = Window_KeyWordItem.new(self)
+    @name_window = Window_MessageName.new(self)
+  end
+  
+  alias :old_dispose_all_windows :dispose_all_windows
+  def dispose_all_windows
+    old_dispose_all_windows
+    @keyword_window.dispose
+    @name_window.dispose
+  end
+  
+  alias :old_update_all_windows :update_all_windows
+  def update_all_windows
+    old_update_all_windows
+    @keyword_window.update
+    @name_window.update
+  end
+  
+  alias :old_open_and_wait :open_and_wait
+  def open_and_wait
+    if !open?
+      @name_window.open
+      old_open_and_wait
+    end
+  end
+  
+  alias :old_close_and_wait :close_and_wait
+  def close_and_wait
+    if !$game_message.continue?
+      @name_window.close
+      old_close_and_wait
+    end
+  end
+  
+  alias :old_process_character :process_character
+  def process_character(c, text, pos)
+    if c == '`'
+      sign_pos = text.index '`'
+      name = text[0...sign_pos]
+      text.slice!(0..sign_pos)
+      @name_window.draw_actor_name(name)
+      old_process_character(text.slice!(0,1), text, pos)
+    else
+      old_process_character(c, text, pos)
+    end
+  end
+
+  alias :old_process_input :process_input
+  def process_input
+    if $game_message.selecting_keyword?
+      input_keyword
+    else
+      old_process_input
+    end
+  end
+
+  def input_keyword
+    @keyword_window.start
+    Fiber.yield while @keyword_window.active
+  end
+end
+
+module RM
+  def self.show_messages messages
+    $game_message.continue = true
+    messages.each{|m| show_message m }
+    $game_message.continue = false
+  end
+
+  def self.show_message message
+    name_code = message[:actor_name] ? "`#{message[:actor_name]}`" : "``"
+    $game_message.add(name_code + message[:content])
+    Fiber.yield while $game_message.busy?
+  end
+
+  def self.select_keyword
+    $game_message.selecting_keyword = true
+    Fiber.yield while $game_message.busy?
+  end
+
+end
+
+module Foo
+  def self.bar
+    $game_message.continue = true
+    a = {content: '123', actor_name: '埃里克'}
+    b = {content: '2332131', actor_name: nil}
+    #c = {content: '2331', actor_name: '123'}
+    RM.show_message a
+    RM.select_keyword
+    RM.show_message b
+    $game_message.continue = false
+  end
+
+end
